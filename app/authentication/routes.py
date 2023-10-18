9242c32b0efa0ae00add8109272b756713c05e6d
ABL <zbyrd72@gmail.com>
2023-10-18 10:13:06 -0400

    added more detailed error logging for register route

--- app/authentication/routes.py
+++ app/authentication/routes.py
@@ -10,25 +10,34 @@ authentication = Blueprint('authentication', __name__, url_prefix='/auth')
 def register():
     data = request.json
 
+    print("Starting registration process...")
+
     # Extract Firebase token from the request data
     token = data.get('token')
+    print("Received token:", token)
 
     # Verify the token with Firebase
     try:
         decoded_token = firebase_auth.verify_id_token(token)
         firebase_uid = decoded_token['uid']
         email = decoded_token['email']
+        print("Decoded Firebase token:", decoded_token)
     except Exception as e:
+        print("Error verifying Firebase token:", str(e))
         return jsonify({"error": "Invalid token", "details": str(e)}), 400
 
     # Check if user already exists in the database using the Firebase UID
     existing_user = User.query.filter_by(firebase_uid=firebase_uid).first()
     if existing_user:
+        print("User already registered with Firebase UID:", firebase_uid)
         return jsonify({"message": "User already registered"}), 200
 
     # Extract other user details from the request data
     first_name = data.get('first_name')
     last_name = data.get('last_name')
+
+    print(
+        f"Received user details - First Name: {first_name}, Last Name: {last_name}")
 
     # Validate if first_name and last_name are provided
     if not first_name or not last_name:
@@ -41,8 +50,10 @@ def register():
     try:
         db.session.add(new_user)
         db.session.commit()
+        print("User added to the database successfully!")
     except Exception as e:
         db.session.rollback()
+        print("Error adding user to the database:", str(e))
         return jsonify({"error": "Database error", "details": str(e)}), 500
 
     return jsonify({"message": "User successfully registered"}), 201
